5ecb916c93f00e3416b0b2675798c1fc85f9b6e1
navalraul <129669942+navalraul@users.noreply.github.com>
2023-05-18 17:08:06 +0530

    update

---
+++ controller/All-controllers.js
@@ -0,0 +1,12 @@
+export const Naval = (req,res) => {
+    res.send("Sending message from Naval function")
+}
+export const Navnath = (req,res) => {
+    res.send("Sending message from Navnath function")
+}
+export const Raul = (req,res) => {
+    res.send("Sending message from Raul function")
+}
+export const Code = (req,res) => {
+    res.send("Sending message from Code function")
+}
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log("Hello World")
-console.log("Naval")
+// console.log("Hello World")
+// console.log("Naval")
+
+import express from "express";
+import { Naval,Navnath,Raul,Code } from "./controller/All-controllers.js";
+
+const app = express();
+
+app.get('/naval',Naval);
+app.get('/navnath',Navnath);
+app.get('/raul',Raul);
+app.get('/code',Code);
+
+app.listen(8000)
