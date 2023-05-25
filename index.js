49438cbe0d16ed2d5e81219e401d0936ce1efba6
navalraul <129669942+navalraul@users.noreply.github.com>
2023-05-25 19:26:56 +0530

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import express from "express";
 import morgan from "morgan";
 import { Naval,Navnath,Raul,Code } from "./controller/All-controllers.js";
 import router, {} from './routes/UserRoutes.js';
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use('/api/v1', router);
 
 
 
-mongoose.connect('mongodb+srv://naval1305:naval1234@cluster0.k0bgc2r.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://naval1305:naval1234@cluster0.k0bgc2r.mongodb.net/')
 .then(() => console.log("DB connected"))
 .catch((err) => console.log("DB Error =>", err));
 
@@ -23,4 +24,4 @@ mongoose.connect('mongodb+srv://naval1305:naval1234@cluster0.k0bgc2r.mongodb.net
 // app.get('/raul',Raul);
 // app.get('/code',Code);
 
-app.listen(8000,()=> console.log("Working on port 8000"))
+app.listen(8001,()=> console.log("Working on port 8000"))
